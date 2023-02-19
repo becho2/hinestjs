1b65259d89f7c6af8de93ced1c23d75f14d2bde5
jinhoe <jinhoe.kim@imweb.me>
2023-02-19 16:08:25 +0900

    feat: develop movie controllers, and make service file

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Param, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    @Get()
+    getAll(){
+        return "This will return all movies";
+    }
+
+    
+    @Get("search")
+    search(@Query('year') searchingYear: string){
+        return `We are searching for a movie made after: ${searchingYear} `;
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') id: string){
+        return `this will return movie id: ${id}`;
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return movieData;
+    }
+
+    @Patch('/:id')
+    update(@Param('id') id: string, @Body() updateData){
+        return {
+            updatedMovie: id,
+            ...updateData,
+        };
+    }
+
+}
