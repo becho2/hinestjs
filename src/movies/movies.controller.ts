b54b1694bc2609d4272b96be59c814c89d5ee8f5
jinhoe <jinhoe.kim@imweb.me>
2023-02-19 16:44:32 +0900

    feat: make movie CRUD logic wihtout real database

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Patch, Param, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
-    @Get()
-    getAll(){
-        return "This will return all movies";
-    }
+    constructor(private readonly MoviesService: MoviesService) {
 
-    
-    @Get("search")
-    search(@Query('year') searchingYear: string){
-        return `We are searching for a movie made after: ${searchingYear} `;
+    }
+    @Get()
+    getAll(): Movie[] {
+        return this.MoviesService.getAll();
     }
 
     @Get('/:id')
-    getOne(@Param('id') id: string){
-        return `this will return movie id: ${id}`;
+    getOne(@Param('id') id: string): Movie {
+        return this.MoviesService.getOne(id);
     }
+    
+    // @Get("search")
+    // search(@Query('year') searchingYear: string){
+    //     return `We are searching for a movie made after: ${searchingYear} `;
+    // }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.MoviesService.create(movieData);
     }
 
     @Patch('/:id')
     update(@Param('id') id: string, @Body() updateData){
-        return {
-            updatedMovie: id,
-            ...updateData,
-        };
+        return this.MoviesService.update(id, updateData);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.MoviesService.deleteOne(id);
     }
 
 }
